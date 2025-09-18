006cdc51facec8047ceec278d44b709fa8b1adb1
pablonudel <pablonudel@gmail.com>
2025-09-18 11:15:03 +0200

    Refactor translations and new CV files

--- src/utils/lang.js
+++ src/utils/lang.js
@@ -4,21 +4,16 @@ export const userLang = (navigator.language || navigator.userLanguage).slice(
 )
 
 const translations = {
-	title: { es: "Desarrollador Web Jr", fr: "Développeur Web Jr" },
+	title: { es: "Desarrollador Web", fr: "Développeur Web" },
 	hi: { es: "Hola! soy", fr: "Salut ! Je suis" },
 	roles: {
 		es: [
-			"Desarrollador Web Jr",
+			"Desarrollador Web",
 			"Ilustrador",
 			"Diseñador Motion Graphics",
 			"Diseñador Gráfico",
 		],
-		fr: [
-			"Développeur Web Jr",
-			"Illustrateur",
-			"Motion Designer",
-			"Designer Graphiste",
-		],
+		fr: ["Développeur Web", "Illustrateur", "Motion Designer", "Graphiste"],
 	},
 	comingSoon: {
 		es: "Próximamente nuevo website!",
@@ -30,15 +25,15 @@ const translations = {
 export const cvLang = {
 	en: {
 		label: "en",
-		file: "./cv/Pablo_Nudel-Jr_Web_Developer.pdf",
+		file: "./cv/Pablo_Nudel-Web_Developer.pdf",
 	},
 	es: {
 		label: "es",
-		file: "./cv/Pablo_Nudel-Desarrollador_Web_Jr.pdf",
+		file: "./cv/Pablo_Nudel-Desarrollador_Web.pdf",
 	},
 	fr: {
 		label: "fr",
-		file: "./cv/Pablo_Nudel-Développeur_Web_Jr.pdf",
+		file: "./cv/Pablo_Nudel-Développeur_Web.pdf",
 	},
 }
 
